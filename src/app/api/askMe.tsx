a62e101440d261ff9628a6a2bdf96d0d1358e931
Ruthskery <cyrilimperial12@gmail.com>
2025-12-13 21:47:23 +0800

    Delete src/app/api/askMe.tsx
    API askMe.tsx removed

--- src/app/api/askMe.tsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export async function askMe(question: string): Promise<string> {
-  const res = await axios.post("http://localhost:8000/ask-me", {
-    message: question,
-  });
-  return res.data.reply;
-}
